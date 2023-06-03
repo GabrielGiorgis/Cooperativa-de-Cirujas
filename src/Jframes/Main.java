d2749b4b2ab6cf8834c0df2e5b3fa534975bfe44
AgusMandolesi <agustin.mandolesi@alumnos.frm.utn.edu.ar>
2023-06-03 16:25:10 -0300

    interfaz añadida

---
+++ src/Jframes/Main.java
@@ -0,0 +1,14 @@
+package Jframes;
+
+import clases.SerCantor;
+import java.util.ArrayList;
+
+public class Main {
+
+    static ArrayList<SerCantor> cantantes = new ArrayList<>();
+
+    public static void main(String[] args) {
+        styledMainFrame main = new styledMainFrame();
+        main.setVisible(true);
+    }
+}
