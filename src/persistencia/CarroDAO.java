efa2c27d48012eb9ae2663923eea955764a7dbcb
Isabel Jurman <116084948+isabeljurman@users.noreply.github.com>
2023-06-05 09:04:44 -0300

    Agrego carpeta de persistencia actualizada

---
+++ src/persistencia/CarroDAO.java
@@ -0,0 +1,108 @@
+package persistencia;
+
+import Clases.Carro;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarroDAO extends DAO {
+
+    // CREATE
+    public void create(int ciruja_id, Carro carro) throws Exception {
+        try {
+            if (carro == null) {
+                throw new Exception("No válido");
+            }
+            String sql = "INSERT INTO Carro(ciruja_id, cargaActual) VALUES(?, ?);";
+            PreparedStatement statement = conexion.prepareStatement(sql);
+            statement.setInt(1, ciruja_id);
+            statement.setDouble(2, carro.getCargaActual());
+
+            System.out.println(sql);
+            agregarModificarEliminar(sql);
+        } catch (Exception e) {
+            throw new Exception("Error al guardar");
+        }
+    }
+
+    // READ ONE
+    public Carro getOne(int idCarro) throws Exception {
+        try {
+            String consulta = "SELECT * FROM Carro WHERE idCarro = " + idCarro + ";";
+
+            consultarBase(consulta);
+
+            Carro carro = null;
+
+            while (resultado.next()) {
+                carro = new Carro();
+
+                carro.setId(resultado.getInt(1));
+                carro.setCargaActual(resultado.getDouble(2));
+            }
+
+            return carro;
+
+        } catch (Exception e) {
+            throw new Exception("Erro al realizar la obtención");
+        } finally {
+            desconectarBase();
+        }
+    }
+
+    // READ ALL
+    public List<Carro> getAll() throws Exception {
+        try {
+            String sql = "SELECT * FROM Carro;";
+            System.out.println(sql);
+
+            consultarBase(sql);
+
+            List<Carro> listaCarros = new ArrayList<>();
+
+            while (resultado.next()) {
+                Carro carro = new Carro();
+
+                carro.setId(resultado.getInt(1));
+                carro.setCargaActual(resultado.getDouble(2));
+
+                listaCarros.add(carro);
+            }
+            return listaCarros;
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            desconectarBase();
+        }
+    }
+
+    // UPDATE
+    public void update(int ciruja_id, Carro carro) throws Exception {
+        try {
+            if (carro == null) {
+                throw new Exception("No válido");
+            }
+            String sql = "UPDATE Carro SET ciruja_id = ? cargaActual = ? WHERE idCarro = " + carro.getId() + ";";
+            PreparedStatement statement = conexion.prepareStatement(sql);
+            statement.setInt(1, ciruja_id);
+            statement.setDouble(2, carro.getCargaActual());
+            System.out.println(sql);
+            agregarModificarEliminar(sql);
+        } catch (Exception e) {
+            throw new Exception("Error al modificar");
+        }
+    }
+
+    // DELETE
+    public void delete(int idCarro) throws Exception {
+        try {
+
+            String query = "DELETE FROM Carro WHERE idCarro = " + idCarro + ";";
+            System.out.println(query);
+            agregarModificarEliminar(query);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+}
---
+++ src/persistencia/DAO.java
@@ -0,0 +1,67 @@
+package persistencia;
+
+import java.sql.*;
+
+public class DAO {
+    
+    protected Connection conexion = null;
+    protected Statement sentencia = null;
+    protected ResultSet resultado = null;
+    
+    private String USUARIO = "root";
+    private String CLAVE = "";
+    private String CONTROLADOR = "com.mysql.cj.jdbc.Driver";
+    private String url = "jdbc:mysql://localhost::3306/cooperativa_cirujas?zeroDateTimeBehavior=CONVERT_TO_NULL";
+    
+    protected void conectarBase() throws ClassNotFoundException, SQLException {
+        try {
+            Class.forName(CONTROLADOR);
+            conexion = DriverManager.getConnection(url, USUARIO, CLAVE);
+        } catch (ClassNotFoundException | SQLException e) {
+            throw e;
+        }
+    }
+    
+    protected void desconectarBase() throws SQLException {
+        try {
+            if (sentencia != null) {
+                sentencia.close();
+            }
+            if (resultado != null) {
+                resultado.close();
+            }
+            if (conexion != null) {
+                conexion.close();
+            }
+        } catch (SQLException e) {
+            throw e;
+        }
+    }
+    
+    protected void agregarModificarEliminar(String sql) throws SQLException, ClassNotFoundException {
+        try {
+            conectarBase();
+            sentencia = conexion.createStatement();
+            sentencia.executeUpdate(sql);
+        } catch (SQLException e) {
+            try {
+                conexion.rollback();
+            } catch (SQLException ex) {
+                throw ex;
+            }
+            throw e;
+        } finally {
+            desconectarBase();
+        }
+    }
+    
+    protected void consultarBase(String sql) throws Exception {
+        try {
+            conectarBase();
+            sentencia = conexion.createStatement();
+            resultado = sentencia.executeQuery(sql);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+}
